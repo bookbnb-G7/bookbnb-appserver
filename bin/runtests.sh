0955662e7bbb117a33dba030d9476818bfce0dd8
facutorraca <ftorraca@fi.uba.ar>
2020-10-18 22:03:16 -0300

    fix app name on database connection check

--- bin/runtests.sh
+++ bin/runtests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 docker-compose up -d
-docker exec bookbnb-userserver_web bash -c 'while !</dev/tcp/db/5432; do sleep 1; done;'
+docker exec bookbnb-appserver_web bash -c 'while !</dev/tcp/db/5432; do sleep 1; done;'
 docker exec bookbnb-appserver_web pytest --cov=appserver --color=yes
 docker exec bookbnb-appserver_web pylint appserver
 docker-compose down
