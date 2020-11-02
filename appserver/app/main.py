af0914cf8b5d870fb4d5f3972a6bdca048a9b971
komod0 <juliancrespo15@gmail.com>
2020-11-02 05:01:10 -0300

    Comment unused stuff

--- appserver/app/main.py
+++ appserver/app/main.py
@@ -1,23 +1,18 @@
 from fastapi import FastAPI
-from app.db import engine, metadata, database
-from app.api.routes import post_router, note_router, user_router
-
-metadata.create_all(engine)
+# from app.db import engine, metadata, database
+from app.api.routes import room_router
 
 app = FastAPI()
 
-@app.on_event("startup")
-async def startup():
-    await database.connect()
 
-@app.on_event("shutdown")
-async def shutdown():
-    await database.disconnect()
+# """@app.on_event("startup")
+# async def startup():
+#     await database.connect()"""
 
-@app.get("/")
-async def pong():
-    return {"message": "appserver"}
 
-app.include_router(user_router.router, prefix="/users", tags=["users"])
-app.include_router(post_router.router, prefix="/posts", tags=["posts"])
-app.include_router(note_router.router, prefix="/notes", tags=["notes"])
+# @app.on_event("shutdown")
+# async def shutdown():
+#     await database.disconnect()"""
+
+
+app.include_router(room_router.router, prefix="/rooms", tags=["rooms"])
