881ed2042586b82bdae53b6ce7eec666dc1a40ca
komod0 <juliancrespo15@gmail.com>
2020-11-02 15:48:00 -0300

    Use response status code

--- appserver/app/api/routes/room_router.py
+++ appserver/app/api/routes/room_router.py
@@ -27,42 +27,49 @@ async def create_room(payload: RoomSchema, response: Response):
 
 
 @router.post("/{room_id}/ratings", response_model=RoomRatingDB)
-async def rate_room(payload: RoomRatingSchema, room_id: int):
+async def rate_room(payload: RoomRatingSchema, room_id: int, response: Response):
     room = rq.post(API_URL + f"/{room_id}/ratings", json=payload.dict())
+    response.status_code = room.status_code
     return room.json()
 
 
 @router.post("/{room_id}/reviews", response_model=RoomReviewDB)
-async def review_room(payload: RoomReviewSchema, room_id: int):
+async def review_room(payload: RoomReviewSchema, room_id: int, response: Response):
     room = rq.post(API_URL + f"/{room_id}/reviews", json=payload.dict())
+    response.status_code = room.status_code
     return room.json()
 
 
 @router.get("/{room_id}", response_model=RoomDB)
-async def get_room(room_id: int):
+async def get_room(room_id: int, response: Response):
     room = rq.get(API_URL + f"/{room_id}")
+    response.status_code = room.status_code
     return room.json()
 
 
 @router.get("/{room_id}/reviews/{review_id}", response_model=RoomReviewDB)
-async def get_room_review(room_id: int, review_id: int):
+async def get_room_review(room_id: int, review_id: int, response: Response):
     room = rq.get(API_URL + f"/{room_id}/reviews/{review_id}")
+    response.status_code = room.status_code
     return room.json()
 
 
 @router.get("/{room_id}/ratings/{rating_id}", response_model=RoomRatingDB)
-async def get_room_rating(room_id: int, rating_id: int):
+async def get_room_rating(room_id: int, rating_id: int, response: Response):
     room = rq.get(API_URL + f"/{room_id}/ratings/{rating_id}")
+    response.status_code = room.status_code
     return room.json()
 
 
 @router.get("/{room_id}/ratings", response_model=RoomRatingList)
-async def get_all_room_ratings(room_id: int):
+async def get_all_room_ratings(room_id: int, response: Response):
     room = rq.get(API_URL + f"/{room_id}/ratings")
+    response.status_code = room.status_code
     return room.json()
 
 
 @router.get("/{room_id}/reviews", response_model=RoomReviewList)
-async def get_all_room_reviews(room_id: int):
+async def get_all_room_reviews(room_id: int, response: Response):
     room = rq.get(API_URL + f"/{room_id}/reviews")
+    response.status_code = room.status_code
     return room.json()
