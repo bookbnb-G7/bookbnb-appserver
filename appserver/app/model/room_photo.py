5c2a5d9063fe79845198c0143da5695bf02d208c
komod0 <juliancrespo15@gmail.com>
2020-12-03 21:12:02 -0300

    Formatting

--- appserver/app/model/room_photo.py
+++ appserver/app/model/room_photo.py
@@ -1,22 +1,22 @@
-from app.db import Base 
-from datetime import datetime
-from sqlalchemy import Column, String, Integer, Float, ForeignKey, DateTime
+from sqlalchemy import Column, Integer
+from app.db import Base
+
 
 class RoomPhoto(Base):
 
-	__tablename__ = 'room_photos'
+    __tablename__ = "room_photos"
 
-	id = Column("id", Integer, primary_key=True)
-	firebase_id = Column(Integer, nullable=False)
-	room_photo_id = Column(Integer, nullable=False)
+    id = Column("id", Integer, primary_key=True)
+    firebase_id = Column(Integer, nullable=False)
+    room_photo_id = Column(Integer, nullable=False)
 
-	def __init__(self, firebase_id, room_photo_id):
-		self.firebase_id = firebase_id
-		self.room_photo_id = room_photo_id
-		
-	def serialize(self):
-		return {
-			'id': self.id,
-			'firebase_id': self.firebase_id,
-			'room_photo_id': self.room_photo_id
-		}
+    def __init__(self, firebase_id, room_photo_id):
+        self.firebase_id = firebase_id
+        self.room_photo_id = room_photo_id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "firebase_id": self.firebase_id,
+            "room_photo_id": self.room_photo_id,
+        }
