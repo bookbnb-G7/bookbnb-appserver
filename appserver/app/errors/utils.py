f8f654a20d7f3aee9d618c24d7c44ad5040e8f29
nicofandos <nfandos@fi.uba.ar>
2021-01-30 14:12:29 -0300

    error print

--- appserver/app/errors/utils.py
+++ appserver/app/errors/utils.py
@@ -1,5 +1,8 @@
+import json
 from requests.models import Response
 
 
 def get_error_message(response: Response):
-    return response.json()["error"]
+    if response.json().error is not None:
+        return response.json()["error"]
+    return "'error' attr not present, err: " + json.dumps(response.json())
