ecaee46066392b3e4d0701e40b56ef2001e2ba13
nicofandos <nfandos@fi.uba.ar>
2021-03-05 00:16:48 -0300

    log

--- appserver/app/services/photouploader.py
+++ appserver/app/services/photouploader.py
@@ -47,6 +47,7 @@ class PhotoUploader:
         while (generate_id and bucket.get_blob(new_filename) is not None):
             img_firebase_id = IdGenerator.generate()
             new_filename = filename + f"{img_firebase_id}"
+            logger.debug("Trying to upload photo with name " + new_filename)
 
         existing_blob = bucket.get_blob(filename)
         if existing_blob is not None:
